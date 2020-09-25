abb9622dba7a4e74466c7da4448161c31066fad5
Alexey Berezin <beraliv.spb@gmail.com>
2020-09-26 03:24:14 +0600

    feat: ➕ add moment card

---
+++ src/components/atoms/MomentCard/index.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { MomentCard, MomentCardParams } from './';
+
+const DEFAULT_MOMENT_CARD_PARAMS: Omit<MomentCardParams, 'size'> = {
+  className: 'storybookMomentCard',
+  time: '01:55:38',
+  title: 'Bayern 1 - 1 Valencia',
+  markColor: '#FF0',
+  phase: 'initial',
+  onPlay: action('play moment'),
+};
+
+storiesOf('components/atoms/MomentCard', module)
+  .addDecorator(storyFn => (
+    <div style={{ position: 'relative', backgroundColor: '#ccc', width: 300 }}>{storyFn()}</div>
+  ))
+  .add('small size', () => <MomentCard size='S' {...DEFAULT_MOMENT_CARD_PARAMS} />)
+  .add('medium size', () => <MomentCard size='M' {...DEFAULT_MOMENT_CARD_PARAMS} />)
+  .add('large size', () => <MomentCard size='L' {...DEFAULT_MOMENT_CARD_PARAMS} />)
+  .add('seekable', () => <MomentCard size='S' {...DEFAULT_MOMENT_CARD_PARAMS} phase='seekable' />)
+  .add('active', () => <MomentCard size='S' {...DEFAULT_MOMENT_CARD_PARAMS} phase='active' />)
---
+++ src/components/atoms/MomentCard/index.tsx
@@ -0,0 +1,69 @@
+import React, { FunctionComponent, memo, useRef } from 'react';
+import classNames from 'classnames';
+import { noop } from '../../../functions/noop';
+import { responsiveClassName } from '../../../functions/responsiveClassName';
+import { SkinSize } from '../../../types/SkinSize';
+import { PlayIcon } from '../PlayIcon';
+import './index.css';
+
+export interface MomentCardParams {
+  className: string;
+  time: string;
+  title: string;
+  markColor: string;
+  size: SkinSize;
+  phase: 'initial' | 'seekable' | 'active';
+  onPlay: VoidFunction;
+}
+
+export const MomentCard: FunctionComponent<MomentCardParams> = memo((props) => {
+  const cardRef = useRef<HTMLDivElement | null>(null);
+
+  const {
+    time,
+    title,
+    phase,
+    className,
+    markColor,
+    size,
+
+    onPlay,
+  } = props;
+
+
+  const handleClick = phase === 'seekable' ? onPlay : noop;
+
+  return (
+    <div
+      className={classNames('momentCard', responsiveClassName(size, {
+        S: 'momentCard_S',
+        M: 'momentCard_M',
+        L: 'momentCard_L',
+      }), className, {
+        momentCard_seekable: phase === 'seekable',
+        momentCard_active: phase === 'active',
+      })}
+      ref={cardRef}
+      role="button"
+      onClick={handleClick}
+    >
+      <p className='momentCard__title'>{title}</p>
+      <p className='momentCard__time_placeholder'>&nbsp;</p>
+      {phase === 'initial' && (
+        <div
+          className='momentCard__mark'
+          style={{ backgroundColor: markColor }}
+        />
+      )}
+      <p className='momentCard__time'>{time}</p>
+      {phase === 'seekable' && (
+        <div className='momentCard__hovered'>
+          <PlayIcon size={size}/>
+          <p className='momentCard__hovered__text'>
+            {`Play current moment`}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+});
